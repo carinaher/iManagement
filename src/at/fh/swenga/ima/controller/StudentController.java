dbef8e12861d20e05982ac3913e355bbf4418080
Daniel Folk <daniel.Folk@edu.fh-joanneum.at>
2016-05-26 12:26:55 +0200

    Liste der Studenten mit echten Werten angelegt

--- src/at/fh/swenga/ima/controller/StudentController.java
+++ src/at/fh/swenga/ima/controller/StudentController.java
@@ -1,12 +1,9 @@
 package at.fh.swenga.ima.controller;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.fluttercode.datafactory.impl.DataFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -33,19 +30,16 @@ public class StudentController {
 		return "studentIndex";
 	}
 
-	@RequestMapping(value = { "/getPage" })
-	public String getPage(Pageable page, Model model) {
-		// Pagenumbers began with 0
-		Page<StudentModel> students = studentRepository.findAll(page);
-		// Page contains the list and additional information
-		model.addAttribute("students", students.getContent());
-		model.addAttribute("studentsPage", students); // send the whole page
-														// Information to the
-														// jsp
-
-		return "index";
-	}
-
+	/*
+	 * @RequestMapping(value = { "/getPage" }) public String getPage(Pageable
+	 * page, Model model) { // Pagenumbers began with 0 Page<StudentModel>
+	 * students = studentRepository.findAll(page); // Page contains the list and
+	 * additional information model.addAttribute("students",
+	 * students.getContent()); model.addAttribute("studentsPage", students); //
+	 * send the whole page // Information to the // jsp
+	 * 
+	 * return "index"; }
+	 */
 	@RequestMapping(value = { "/find" })
 	// search for Requestparameter with the name type and store it in the string
 	// type
@@ -79,14 +73,37 @@ public class StudentController {
 	@Transactional
 	public String fillData(Model model) {
 
-		// Creates always the same data
-		DataFactory df = new DataFactory();
-
-		for (int i = 0; i < 100; i++) {
-			StudentModel sm = new StudentModel(df.getFirstName(), df.getFirstName(), df.getLastName(), df.getLastName(),
-					df.getEmailAddress(), df.getNumberBetween(2013, 2016), df.getNumberBetween(1, 4));
-			studentRepository.save(sm);
-		}
+		List<StudentModel> students = new ArrayList<>();
+		students.add(new StudentModel("Folkdani13", "Daniel", "Folk", "Folkdani13", "daniel.folk@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("Seebacha14", "Andreas", "Seebacher", "Andseeb","andreas.seebacher@edu-fh.joanneum.at", 14, 1));
+		students.add(new StudentModel("Lichtene05", "Alexander", "Lichtenegger", "AlexanderLichtenegger","alexander.lichtenegger@edu-fh.joanneum.at", 14, 1));
+		students.add(new StudentModel("Skerbinz13", "Verena", "Skerbinz", "Verisker","verena.skerbinz@edu-fh.joanneum.at", 14, 1));
+		students.add(new StudentModel("Herzocar14", "Carina", "Herzog", "Carinaher", "carina.herzog@edu-fh.joanneum.at",14, 1));
+		
+		students.add(new StudentModel("bajricam14", "Amar", "Bajric", "amarbajric", "Amar.Bajric@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("fuchsmic14", "Michael", "Fuchs", "deKilla", "Michael.Fuchs@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("graffeli13", "Felix", "Graf", "Graf-Carello", "Felix.Graf@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("hasenbic13", "Timo", "Hasenbichler", "timoooo", "Timo.Hasenbichler@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("hoxahagra14", "Granit", "Hoxha", "hoxhagra14", "Granit.Hoxha@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("hysistev14", "Steven", "Hysi", "lionade", "Steven.Hysi@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("kandlhof14", "Daniel", "Kandlhofer", "danielkandlhofer", "Daniel.Kandlhofer@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("karimova13", "Elza", "Karimova", "elsakarimova", "Elza.Karimova@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("knallerm14", "Markus", "Knaller", "knalla66", "Markus.Knaller@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("koernerp13", "Paul", "Körner", "mcKorleone", "Paul.Körner@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("laggerch14", "Christian", "Lagger", "clagger", "Christian.Lagger@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("leitners07", "Stefan", "Leitner", "loete", "Stefan.Leitner@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("meizenit14", "Georg", "Meizenitsch", "meizenit14", "Georg.Meizenitsch@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("ortmann14", "Thomas", "Ortmann", "tortmann", "Thomas.Ortmann@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("rexhajrr13", "Rrolf", "Rexhaj", "rexhajrr13", "Rrolf.Rexhaj@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("schneida13", "Andreas", "Schneider", "zerberuss", "Andreas.Schneider2@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("spalekni", "Nina", "Spalek", "NSpalek", "Nina.Spalek@edu-fh.joanneum.at",14, 1));
+		students.add(new StudentModel("spanning13", "Florian", "Spanninger", "spanning13", "Florian.Spanninger@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("steinkel14", "Wolfgang", "Steinkellner", "steinkel14", "Wolfgang.Steinkellner@edu-fh.joanneum.at",14, 2));
+		students.add(new StudentModel("wagenede14", "Maximilian", "Wageneder", "mwageneder", "Maximilian.Wageneder@edu-fh.joanneum.at",14, 1));
+		
+		
+		
+		studentRepository.save(students);
 
 		return "forward:list";
 	}
