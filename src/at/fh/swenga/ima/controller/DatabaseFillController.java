f928dbbfc0cb15a47f02e4421dd591a99ab9fb37
AlexanderLichtenegger <alexander.lichtenegger@edu.fh-joanneum.at>
2016-06-19 18:17:32 +0200

    Fill Database Controller edited

--- src/at/fh/swenga/ima/controller/DatabaseFillController.java
+++ src/at/fh/swenga/ima/controller/DatabaseFillController.java
@@ -1,12 +1,11 @@
 package at.fh.swenga.ima.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import at.fh.swenga.ima.dao.UserRepository;
 import at.fh.swenga.ima.dao.UserRoleRepository;
@@ -20,51 +19,53 @@ public class DatabaseFillController {
 	@Autowired
 	UserRoleRepository userRoleRepository;
 
-
 	@RequestMapping(value = { "/fillDatabase", "fill" })
 	public String fill() {
-		
-		// users
-		User[] users = new User[] {
-				new User("seebacha14", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("lichtene05", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("skerbinz13", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("herzocar14", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("folkdani13", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				
-				new User("admin", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("user", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true),
-				new User("user2", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true)
-		};
-		userRepository.save(Arrays.asList(users));
-		
-		// user roles
-		UserRole[] roles = new UserRole[] {
-				new UserRole(userRepository.findByUserName("seebacha14"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("seebacha14"), "ROLE_STUDENT"),
-				new UserRole(userRepository.findByUserName("lichtene05"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("lichtene05"), "ROLE_STUDENT"),
-				new UserRole(userRepository.findByUserName("skerbinz13"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("skerbinz13"), "ROLE_STUDENT"),
-				new UserRole(userRepository.findByUserName("herzocar14"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("herzocar14"), "ROLE_STUDENT"),
-				new UserRole(userRepository.findByUserName("folkdani13"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("folkdani13"), "ROLE_STUDENT"),
-				
-				new UserRole(userRepository.findByUserName("admin"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("admin"), "ROLE_ADMIN"),
-				
-				new UserRole(userRepository.findByUserName("user"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("user"), "ROLE_STUDENT"),
-				
-				new UserRole(userRepository.findByUserName("user2"), "ROLE_USER"),
-				new UserRole(userRepository.findByUserName("user2"), "ROLE_STUDENT")
-		};
-		userRoleRepository.save(Arrays.asList(roles));
-		
-		
+
+		// admin
+		userRepository.save(new User("admin", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true));
+		userRoleRepository.save(new UserRole(userRepository.findByUserName("admin"), "ROLE_USER"));
+		userRoleRepository.save(new UserRole(userRepository.findByUserName("admin"), "ROLE_ADMIN"));
+
+		// demo user (no student)
+		userRepository.save(new User("user", "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true));
+		userRoleRepository.save(new UserRole(userRepository.findByUserName("user"), "ROLE_USER"));
+
+		// students
+		List<String> students = new ArrayList<String>();
+		students.add("folkdani13");
+		students.add("seebacha14");
+		students.add("lichtene05");
+		students.add("skerbinz13");
+		students.add("herzocar14");
+		students.add("bajricam14");
+		students.add("fuchsmic14");
+		students.add("graffeli13");
+		students.add("hasenbic13");
+		students.add("hoxahagra14");
+		students.add("hysistev14");
+		students.add("kandlhof14");
+		students.add("karimova13");
+		students.add("knallerm14");
+		students.add("koernerp13");
+		students.add("laggerch14");
+		students.add("leitners07");
+		students.add("meizenit14");
+		students.add("ortmann14");
+		students.add("rexhajrr13");
+		students.add("schneida13");
+		students.add("spalekni14");
+		students.add("spanning13");
+		students.add("steinkel14");
+		students.add("wagenede14");
+
+		for (String student : students) {
+			userRepository.save(new User(student, "$2a$10$2BZh7qw/FSh23ZCbojA.OOoo7vzg7KaqHUp34l8/i9.ktxzcr3vJm", true));
+			userRoleRepository.save(new UserRole(userRepository.findByUserName(student), "ROLE_USER"));
+			userRoleRepository.save(new UserRole(userRepository.findByUserName(student), "ROLE_STUDENT"));
+		}
 
 		return "databaseFilled";
 	}
-	
+
 }
