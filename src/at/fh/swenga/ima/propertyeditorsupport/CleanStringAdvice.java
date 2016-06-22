9f6396067a37879f97609f06672601cba13b9e73
Andreas Seebacher <andreas.seebacher@outlook.at>
2016-06-22 17:31:35 +0200

    added jsoup sanitizer

---
+++ src/at/fh/swenga/ima/propertyeditorsupport/CleanStringAdvice.java
@@ -0,0 +1,34 @@
+package at.fh.swenga.ima.propertyeditorsupport;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.beans.PropertyEditorSupport;
+
+@ControllerAdvice
+public class CleanStringAdvice {
+	// http://scrumbucket.org/sanitize-your-spring-mvc-controller/
+	static public class StringCleaner extends PropertyEditorSupport {
+		@Override
+		public void setAsText(String text) {
+			if (text == null) {
+				setValue(null);
+			}
+			else {
+				String safe = Jsoup.clean(text, Whitelist.simpleText());
+				setValue(safe);
+			}
+		}
+
+	}
+
+	@InitBinder
+	public void bindStringCleaner(WebDataBinder webDataBinder) {
+		StringCleaner stringCleaner = new StringCleaner();
+		webDataBinder.registerCustomEditor(String.class, stringCleaner);
+	}
+
+}
